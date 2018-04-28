5ebe17e10a2e72a0436aa342cb5731e53c27f03d
pluspunkt <pluspunkt@protonmail.com>
2018-04-28 23:42:20 +0200

    Added and tested a version of the Encryptor. Encrypting a text and writing it down works fine.

---
+++ encryptor.py
@@ -0,0 +1,44 @@
+import time
+
+def nextAlphabetLetter(text = "test"):
+    letters = ["a","b","c","d","e","f","g","h","i","j","k","l","m","n","o","p","q","r","s","t","u","v","w","x","y","z","a","A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","Q","Y","Z","A"]
+    newText = " "
+    foundMatch = False
+
+    for l in range(len(text)):
+
+        for a in range(len(letters)):
+            if text[l] == letters[a]:
+                newText += letters[a + 1]
+                foundMatch = True
+                break
+
+        if foundMatch == False:
+            newText += text[l]
+
+        foundMatch = False
+
+    return newText
+
+
+testText = "abc"
+
+def writeToFile(text, filename = "scrtxt.txt"):
+    txt = open(filename, "w")
+    txt.write(text)
+    txt.close()
+
+def readFromFile(filename = "txt.txt", printInfo = False):
+    txt = open(filename, "r")
+    content = " "
+    counter = 1
+    for i in (txt):
+        content += txt.readline()
+        if printInfo == True: print("Successfully read line", counter)
+        counter += 1
+        time.sleep(0.003)
+    txt.close()
+
+    return content
+
+writeToFile(nextAlphabetLetter(readFromFile("txt.txt", True)))
